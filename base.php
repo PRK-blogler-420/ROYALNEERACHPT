eca8c0ddaca4aa331ed45b9197ef4289cf16e825
gnana sai ramakrishna <gnanasairamakrishna2333@gmail.com>
2026-03-27 19:13:18 +0530

    add files

---
+++ base.php
@@ -0,0 +1,33 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "water_order";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get form data
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$quantity = $_POST['quantity'];
+$address = $_POST['address'];
+$address_link = $_POST['address_link'];
+
+// Insert query
+$sql = "INSERT INTO orders (name, phone, quantity, address, address_link)
+VALUES ('$name', '$phone', '$quantity', '$address', '$address_link')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Order submitted successfully";
+} else {
+    echo "Error: " . $conn->error;
+}
+
+$conn->close();
